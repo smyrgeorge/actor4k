4d4590eda6d3ce8843020fe52dd90c17c579be45
George Smyrnaios <smyrgeorge@gmail.com>
2024-01-07 21:51:16 +0100

    Publish to github packages.

--- actor4k/build.gradle.kts
+++ actor4k/build.gradle.kts
@@ -84,7 +84,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/io.github.smyrgeorge/actor4k")
+            url = uri("https://maven.pkg.github.com/smyrgeorge/actor4k")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
