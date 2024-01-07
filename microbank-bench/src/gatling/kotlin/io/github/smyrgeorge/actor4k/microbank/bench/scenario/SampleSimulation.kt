cf6d42b690118f057d08326e107b41da4eecc963
George Smyrnaios <smyrgeorge@gmail.com>
2024-01-07 10:16:44 +0100

    Added 'microbank :: get single account.jmx'

--- microbank-bench/src/gatling/kotlin/io/github/smyrgeorge/actor4k/microbank/bench/scenario/SampleSimulation.kt
+++ microbank-bench/src/gatling/kotlin/io/github/smyrgeorge/actor4k/microbank/bench/scenario/SampleSimulation.kt
@@ -16,33 +16,27 @@ class SampleSimulation : Simulation() {
     private fun randomAccount() = accounts[Random.nextInt(1, numOfAccounts)]
 
     @Language("json")
-    fun bodyOf(accountNo: String) =
-        """{"accountNo": "$accountNo", "value": 10}"""
+    fun bodyOf(accountNo: String) = """{"accountNo": "$accountNo", "value": 10}"""
 
-    private val httpProtocol = http
-        .baseUrl("http://localhost:9000/api/account")
+    private val httpProtocol = http.baseUrl("http://localhost:9000/api/account")
 
-    private val req =
-        http("tx")
-            .post { s ->
-                val accountNo = s.get<String>("accountNo")!!
-                "/$accountNo"
-            }
-            .body(StringBody { s ->
-                val accountNo = s.get<String>("accountNo")!!
-                bodyOf(accountNo)
-            })
+    private val req = http("tx")
+        .post {
+            val accountNo = it.get<String>("accountNo")!!
+            "/$accountNo"
+        }.body(StringBody {
+            val accountNo = it.get<String>("accountNo")!!
+            bodyOf(accountNo)
+        })
 
-    private val scenario =
-        scenario(SampleSimulation::class.java.simpleName)
-            .exec { s -> s.set("accountNo", randomAccount()) }
-            .exec(req)
+    private val scenario = scenario(SampleSimulation::class.java.simpleName)
+        .exec { s -> s.set("accountNo", randomAccount()) }
+        .exec(req)
 
     init {
         setUp(
             scenario.injectClosed(
-                rampConcurrentUsers(10).to(500)
-                    .during(30.seconds.toJavaDuration())
+                rampConcurrentUsers(10).to(100).during(30.seconds.toJavaDuration())
             )
         ).protocols(httpProtocol)
     }
