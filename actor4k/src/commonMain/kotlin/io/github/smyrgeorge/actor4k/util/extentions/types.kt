c0a171055bf25300f60b2193595b5012a11a1d7c
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2025-03-09 20:20:58 +0100

    Remove internal modifier from typealias definitions
    The `internal` modifier was removed from `AnyActor`, `AnyActorClass`, and `ActorFactory` typealias definitions. This change makes these type aliases publicly accessible for broader use across modules or external libraries.

--- actor4k/src/commonMain/kotlin/io/github/smyrgeorge/actor4k/util/extentions/types.kt
+++ actor4k/src/commonMain/kotlin/io/github/smyrgeorge/actor4k/util/extentions/types.kt
@@ -3,6 +3,6 @@ package io.github.smyrgeorge.actor4k.util.extentions
 import io.github.smyrgeorge.actor4k.actor.Actor
 import kotlin.reflect.KClass
 
-internal typealias AnyActor = Actor<*, *>
-internal typealias AnyActorClass = KClass<out AnyActor>
-internal typealias ActorFactory = (key: String) -> AnyActor
+typealias AnyActor = Actor<*, *>
+typealias AnyActorClass = KClass<out AnyActor>
+typealias ActorFactory = (key: String) -> AnyActor
