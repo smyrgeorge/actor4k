d38611c8081ed8a1cf1671739c7516dd4d513617
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2025-03-20 09:45:54 +0100

    Upgrade to version 0.54.0 and add inline optimizations.
    Updated project version to 0.54.0. Applied `inline` optimizations to `SimpleLogger` methods across platforms for better performance. Adjusted `logger` property visibility and added relevant suppress warnings.

--- actor4k/src/wasmWasiMain/kotlin/io/github/smyrgeorge/actor4k/util/SimpleLogger.kt
+++ actor4k/src/wasmWasiMain/kotlin/io/github/smyrgeorge/actor4k/util/SimpleLogger.kt
@@ -2,16 +2,16 @@ package io.github.smyrgeorge.actor4k.util
 
 import io.github.smyrgeorge.log4k.Logger as Log4kLogger
 
-@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING", "NOTHING_TO_INLINE", "OVERRIDE_BY_INLINE")
 actual class SimpleLogger(
-    private val logger: Log4kLogger
+    val logger: Log4kLogger
 ) : Logger {
-    actual override fun debug(msg: String): Unit = logger.debug(msg)
-    actual override fun debug(msg: String, vararg args: Any): Unit = logger.debug(msg, *args)
-    actual override fun info(msg: String): Unit = logger.info(msg)
-    actual override fun info(msg: String, vararg args: Any): Unit = logger.info(msg, *args)
-    actual override fun warn(msg: String): Unit = logger.warn(msg)
-    actual override fun warn(msg: String, vararg args: Any): Unit = logger.warn(msg, *args)
-    actual override fun error(msg: String): Unit = logger.error(msg)
-    actual override fun error(msg: String, e: Throwable): Unit = logger.error(msg, e)
+    actual override inline fun debug(msg: String): Unit = logger.debug(msg)
+    actual override inline fun debug(msg: String, vararg args: Any): Unit = logger.debug(msg, *args)
+    actual override inline fun info(msg: String): Unit = logger.info(msg)
+    actual override inline fun info(msg: String, vararg args: Any): Unit = logger.info(msg, *args)
+    actual override inline fun warn(msg: String): Unit = logger.warn(msg)
+    actual override inline fun warn(msg: String, vararg args: Any): Unit = logger.warn(msg, *args)
+    actual override inline fun error(msg: String): Unit = logger.error(msg)
+    actual override inline fun error(msg: String, e: Throwable): Unit = logger.error(msg, e)
 }
