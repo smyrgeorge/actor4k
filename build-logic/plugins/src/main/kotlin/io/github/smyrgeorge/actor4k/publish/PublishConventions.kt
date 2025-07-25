51275040669c83084ded89a7d3ae4fe7dfc45d59
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2025-07-25 12:46:38 +0200

    Load Maven Central and signing credentials from `local.properties`, remove redundant Sonatype host parameter.

--- build-logic/plugins/src/main/kotlin/io/github/smyrgeorge/actor4k/publish/PublishConventions.kt
+++ build-logic/plugins/src/main/kotlin/io/github/smyrgeorge/actor4k/publish/PublishConventions.kt
@@ -3,10 +3,12 @@ package io.github.smyrgeorge.actor4k.publish
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinMultiplatform
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
-import com.vanniktech.maven.publish.SonatypeHost
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.internal.extensions.core.extra
 import org.gradle.kotlin.dsl.configure
+import java.io.File
+import java.util.*
 
 @Suppress("unused")
 class PublishConventions : Plugin<Project> {
@@ -15,6 +17,23 @@ class PublishConventions : Plugin<Project> {
         "actor4k" to "A small actor system written in kotlin using Coroutines (kotlinx.coroutines).",
         "actor4k-cluster" to "A small actor system written in kotlin using Coroutines (kotlinx.coroutines).",
     )
+
+    private fun Project.loadPublishProperties() {
+        val local = Properties()
+        File(project.rootProject.rootDir, "local.properties").also {
+            if (!it.exists()) return@loadPublishProperties
+            it.inputStream().use { s -> local.load(s) }
+        }
+
+        // Set Maven Central credentials as project properties
+        local.getProperty("mavenCentralUsername")?.let { project.extra["mavenCentralUsername"] = it }
+        local.getProperty("mavenCentralPassword")?.let { project.extra["mavenCentralPassword"] = it }
+
+        // Set signing properties as project properties
+        local.getProperty("signing.keyId")?.let { project.extra["signing.keyId"] = it }
+        local.getProperty("signing.password")?.let { project.extra["signing.password"] = it }
+        local.getProperty("signing.secretKeyRingFile")?.let { project.extra["signing.secretKeyRingFile"] = it }
+    }
 
     override fun apply(project: Project) {
         project.plugins.apply("com.vanniktech.maven.publish")
@@ -63,7 +82,7 @@ class PublishConventions : Plugin<Project> {
             }
 
             // Configure publishing to Maven Central
-            publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+            publishToMavenCentral()
 
             // Enable GPG signing for all publications
             signAllPublications()
